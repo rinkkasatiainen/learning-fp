7e434e37fb3773333966e696f21618c083739ed5
Aki Salmi <aki.salmi@iki.fi>
2021-01-04 21:34:41 +0200

    ✨: experimenting with 'State', and it looks great

--- monad-book/src/state.ts
+++ monad-book/src/state.ts
@@ -3,6 +3,7 @@ export type State<A, S> = (x: S) => Tuple<A, S>
 
 export const pure = <A, S>(x: A): State<A, S> => i => [x, i]
 
+export type NextFunction<A, B, S> = (a: A) => State<B, S>
 export const next: <A, B, S>(s: State<A, S>) => (g: (a: A) => State<B, S>) => State<B, S> =
     <A, B, S>(f: State<A, S>) => (g: (a: A) => State<B, S>) => (originalCounter: S) => {
         const [ss, nextCounter]: Tuple<A, S> = f(originalCounter)
--- monad-book/test/discovering-monads/5-flatten-state.test.ts
+++ monad-book/test/discovering-monads/5-flatten-state.test.ts
@@ -1,12 +1,31 @@
 import {expect} from 'chai'
-import {next, pure, State, Tuple} from '../../src/state'
+import {next, NextFunction, pure, State, Tuple} from '../../src/state'
 import {identity} from '../../src/maybe/identity'
 
 describe('Two for the Price of One', () => {
 
+    describe('State', () => {
+
+        it('can call next', () => {
+            const keys = ['first', 'second', 'third']
+
+            const s: State<{ key: string }, number> = pure({key: 'start'})
+            const f: NextFunction<{ key: string }, { key: string }, number> =
+                state => i => [{key: `${state.key}-${keys[i]}`}, ++i]
+
+            expect(s(0)).to.eql([{key: 'start'}, 0])
+            expect(next(s)(f)(0)).to.eql([{key: 'start-first'}, 1])
+            const ss = next<{key: string}, {key: string}, number>(s)(f)
+            expect(next(ss)(f)(0)).to.eql([{key: 'start-first-second'}, 2])
+
+            const sss = next<{key: string}, {key: string}, number>(ss)(f)
+            expect(next(sss)(f)(0)).to.eql([{key: 'start-first-second-third'}, 3])
+        })
+    })
+
     describe('can flatten state', () => {
         const flatten: <A, S>(ss: State<State<A, S>, S>) => State<A, S> =
-            <A, S>(ss: State<State<A, S>, S>) => next<State<A,S>, A, S>(ss)(identity)
+            <A, S>(ss: State<State<A, S>, S>) => next<State<A, S>, A, S>(ss)(identity)
 
         it('works', () => {
             const s1: State<string, number> = (i) => ['any', i]
