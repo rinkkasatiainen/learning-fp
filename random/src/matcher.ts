6fe369147ae08dda3e63a5c7b055fc7a9e6481c2
Aki Salmi <aki.salmi@iki.fi>
2022-02-22 20:06:13 +0200

    :label: Update matcher types

--- random/src/matcher.ts
+++ random/src/matcher.ts
@@ -17,6 +17,6 @@ type PatternMap<K extends PatternKeys, A, B> = TypeMatcherMap<K, A, B>
 export type Pattern<K extends PatternKeys, A, B> = {
     [key in K]: (shape: PatternMap<K, A, B>[key]) => B
 }
-export const matcher: <K extends PatternKeys, A, B>(pattern: Pattern<K, A, B>) => (shape: PatternMatchingType<K>) => B =
+export const matcher: <K extends PatternKeys, A extends PatternMatchingType<K>, B>(pattern: Pattern<K, A, B>) => (shape: A) => B =
     // The type casting seems to work. As an alternative to 'any'
     pattern => shape => pattern[shape._type](shape as any)
